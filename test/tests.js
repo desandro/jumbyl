89dda9d993557361d94dc765b480a9d834ae28b2
David DeSandro <david@desandro.com>
2012-09-16 10:48:35 -0400

    use getConfig in auth.js

---
+++ test/tests.js
@@ -0,0 +1,24 @@
+/*jshint devel: true, node: true */
+
+'use strict';
+
+// -------------------------- modules -------------------------- //
+
+var assert = require('assert');
+
+var jumbyl = require('../lib/jumbyl');
+
+// -------------------------- tests -------------------------- //
+
+// console.log( jumbyl );
+
+// var config = jumbyl.getConfig()
+// console.log( config );
+
+jumbyl.auth();
+
+// Will pass
+// assert.ok(true);
+
+// Will throw an exception
+// assert.ok(false);
