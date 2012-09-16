5153a4903ea474888d851006f44a5505adc9b09c
David DeSandro <david@desandro.com>
2012-09-16 14:15:38 -0400

    getFileParts with tests

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,8 @@
 // -------------------------- modules -------------------------- //
 
 var assert = require('assert');
+var should = require('should');
+var fs = require('fs');
 
 var jumbyl = require('../lib/jumbyl');
 
@@ -15,10 +17,28 @@ var jumbyl = require('../lib/jumbyl');
 // var config = jumbyl.getConfig()
 // console.log( config );
 
-jumbyl.auth();
+// jumbyl.auth();
 
 // Will pass
 // assert.ok(true);
 
 // Will throw an exception
 // assert.ok(false);
+
+// jumbyl.post('test/dummy/command-file-double-click.mdown')
+
+
+// -------------------------- getFileParts -------------------------- //
+
+// no-yaml.mdown has a `---` but does not have actual YAML
+var fileParts;
+var noYamlFilePath = 'test/dummy/no-yaml.mdown';
+
+fileParts = jumbyl.getFileParts( noYamlFilePath );
+fileParts.should.not.have.property('frontMatter');
+fileParts.should.have.property('content');
+// content should match actual file contents
+var noYamlContent = fs.readFileSync( noYamlFilePath, 'utf8' );
+fileParts.content.should.equal( noYamlContent );
+
+
